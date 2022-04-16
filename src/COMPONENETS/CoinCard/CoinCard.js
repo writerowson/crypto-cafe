0b7e2807a03ecbdcc61ab829b5b11af16dc2b8d8
coderowson <runurowson@gmail.com>
2022-04-16 16:32:28 +0600

    coins

--- src/COMPONENETS/CoinCard/CoinCard.js
+++ src/COMPONENETS/CoinCard/CoinCard.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const CoinCard = () => {
+const CoinCard = ({ coin }) => {
+
     return (
-        <div>
+        <div className='shadow-lg rounded-2xl w-[250px] bg-white p-4'>
+            <Link to='/coins'></Link>
+            <div className='flex gap-4 justify-between items-center'>
+                <div className='flex-shrink-0'>
+                    <img className='mx-auto object-cover rounded-full h-16 w-16' src={coin.image} alt="coinImg" /></div>
+                <div className='flex flex-col justify-end'>
+                    <span className='text-gray-600 font-medium'>{coin.name}</span>
+                    <span className='text-gray-600 text-xs'>{coin.symbol}</span>
+                </div>
+            </div>
+
 
         </div>
     );
--- src/COMPONENETS/Coins/Coins.js
+++ src/COMPONENETS/Coins/Coins.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CoinCard from '../CoinCard/CoinCard';
 
 const Coins = () => {
+
+    const [coins, setCoins] = useState([])
+    useEffect(() => {
+        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false`)
+            .then(res => res.json())
+            .then(data => {
+                setCoins(data)
+                console.log(data)
+            })
+    }, [])
     return (
-        <div>
-            <h1>Coins page</h1>
+        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+            <p className='text-center text-3xl font-bold text-gray'>Available Crypto Currencies</p>
+            <p className='text-center mb-12 text-xl font-normal text-gray-500'>Coins : {coins.length}</p>
+            <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center'>
+                {coins.map(coin =>
+                (<CoinCard
+                    key={coin.id}
+                    coin={coin}
+                ></CoinCard>)
+                )}
+            </div>
         </div>
     );
 };
